e07d00fd82b2b57a6062a17b3076de1122200a74
Raviteja swayampu <ravitejaswayampu@outlook.com>
2017-09-18 22:22:36 +0530

    solution created..

---
+++ AsynchronousFileUpload/AsynchronousFileUpload/Models/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsynchronousFileUpload.Models
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> files)
+        {
+            int filecount = 0;
+            string msg = "";
+            try
+            {
+                if (files != null)
+                {
+                    foreach (var file in files)
+                    {
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            var filename = Path.GetExtension(file.FileName);
+                            var path = Path.Combine(Server.MapPath("~/Files"),filename);
+                            file.SaveAs(path);
+                            filecount++;
+                        }
+                    }
+                    msg = filecount + " Files Uploaded Successfully";
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
